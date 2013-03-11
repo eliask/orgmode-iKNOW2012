1531b12e44cbf37483f825e30a241e51ce22b7a8
Elias Kunnas <elias.kunnas@gmail.com>
2013-03-11 15:40:20 +0200

    Incomplete attempt at making the document build with org-mode 7.9.3f
    This makes the required changes to at least make it possible to invoke
    the Makefile. As with the org-unstable branch, el-get is used to get
    the appropriate version of org-mode.
    
     However, the makefile stops with the following:
      Wrong type argument: stringp, nil
    Attempting to manually export the file yields another error:
      org-babel-execute:emacs-lisp: Invalid read syntax: "#"

--- init-new.el
+++ init-new.el
@@ -1,15 +1,17 @@
+(setq el-get-dir (file-truename ".emacs.d/el-get"))
+(add-to-list 'load-path (file-truename ".emacs.d/el-get/el-get"))
+(load-file "el-get-install.el")
 
-(add-to-list 'load-path "~/.emacs.d/org-mode/contrib/lisp") ;; <- adjust
-(add-to-list 'load-path "~/.emacs.d/org-mode/lisp") ;; <- adjust
-(add-to-list 'load-path "~/.emacs.d/org-mode") ;; <- adjust
+(setq el-get-sources
+      (list (append '(:branch "release_7.9.3f"
+                      :shallow t
+                     ) (el-get-read-recipe 'org-mode))))
+(el-get 'sync (mapcar 'el-get-source-name el-get-sources))
 
 (require 'org-install)
 (require 'org-export)
 (require 'org-e-latex)
-
-;; this line only required until the upcomming Org-mode/Emacs24 sync
-(load "~/.emacs.d/org-mode/lisp/org-exp-blocks.el")
-
+(require 'org-exp-blocks)
 
 ;; Configure Babel to support all languages included in the manuscript
 (org-babel-do-load-languages
@@ -26,30 +28,31 @@
 
 ;; Configure Org-mode
   (setq org-export-latex-hyperref-format "\\ref{%s}")
-  (setq org-entities-user nil) 
+  (setq org-entities-user nil)
   (add-to-list 'org-entities-user '("space" "\\ " nil " " " " " " " "))
   (setq org-e-latex-pdf-process '("texi2dvi --clean --verbose --batch %f"))
+  (setq org-export-latex-classes nil)
   (setq org-export-latex-packages-alist nil)
   (add-to-list 'org-export-latex-packages-alist '("" "hyperref"))
   (add-to-list 'org-export-latex-packages-alist '("" "graphicx"))
 
   (require 'org-special-blocks)
 
-(org-add-link-type 
+(org-add-link-type
    "cite" nil
    (lambda (path desc format)
      (cond
       ((eq format 'latex)
              (format "\\cite{%s}" path)))))
 
-(org-add-link-type 
+(org-add-link-type
    "acm" nil
    (lambda (path desc format)
      (cond
       ((eq format 'latex)
              (format "{\\%s{%s}}" path desc)))))
 
-(add-to-list 'org-e-latex-classes
+(add-to-list 'org-export-latex-classes
                '("acm-proc-article-sp"
                  "\\documentclass{acm_proc_article-sp}
               [NO-DEFAULT-PACKAGES]
